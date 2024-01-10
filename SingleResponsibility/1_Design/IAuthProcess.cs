5c3aed2246f881bef3c129b45ada4c6cfa15afc6
Rahul Awasthi <awasthir314@gmail.com>
2024-01-10 16:18:17 +0530

    Single Responsibility

---
+++ SingleResponsibility/1_Design/IAuthProcess.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibility._1_Design
+{
+    internal interface IAuthProcess
+    {
+        bool Login(string username, string password);
+        bool Register(string username, string password);
+        bool LogError(string message);
+        bool SendVerificationEmail(string email);
+    }
+}
+
+// Here the whole authentication process is described as an interface
+// This code is tightly coupled.
---
+++ SingleResponsibility/3_Solution/IUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibility._3_Solution
+{
+    internal interface IUser
+    {
+        bool Login(string username, string password);
+        bool Register(string username, string password);
+    }
+}
